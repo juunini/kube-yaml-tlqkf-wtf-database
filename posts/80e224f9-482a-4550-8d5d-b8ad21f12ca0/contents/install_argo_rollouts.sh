bc4cd753fa43506f6b1f06b445d7b2e3c90da607
Kube YAML tlqkf wtf <juuni.ni.i@gmail.com>
2023-05-05 13:53:56 +0000

    Commit by `Kube YAML tlqkf wtf`

--- posts/80e224f9-482a-4550-8d5d-b8ad21f12ca0/contents/install_argo_rollouts.sh
+++ posts/80e224f9-482a-4550-8d5d-b8ad21f12ca0/contents/install_argo_rollouts.sh
@@ -2,5 +2,5 @@ kubectl create namespace argo-rollouts
 kubectl apply -n argo-rollouts -f https://github.com/argoproj/argo-rollouts/releases/latest/download/install.yaml
 kubectl rollout status deployment -n argo-rollouts
 
-echo -e "Argo Workflows: https://localhost:2746"
-kubectl -n argo port-forward deployment/argo-server 2746:2746 &
+echo -e "Argo Workflows: https://localhost:$ARGO_WORKFLOW_PORT"
+kubectl -n argo port-forward deployment/argo-server $ARGO_WORKFLOW_PORT:2746 &
--- posts/80e224f9-482a-4550-8d5d-b8ad21f12ca0/contents/install_argo_workflows.sh
+++ posts/80e224f9-482a-4550-8d5d-b8ad21f12ca0/contents/install_argo_workflows.sh
@@ -1,6 +1,7 @@
 kubectl create namespace argo
 kubectl apply -n argo -f https://github.com/argoproj/argo-workflows/releases/download/v3.4.4/install.yaml
 kubectl rollout status deployment -n argo
+# If you production, delete under lines
 kubectl patch deployment \
   argo-server \
   --namespace argo \
